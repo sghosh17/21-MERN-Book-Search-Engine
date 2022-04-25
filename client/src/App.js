a3e73c977f1f4c64524dc29e97bad665f883428b
Sampreeti Das <sampreetighosh@gmail.com>
2022-04-25 20:10:36 +0100

    Update to the App Js file

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,10 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Routes,
+  Route,
+} from "react-router-dom";
 import {
   ApolloClient,
   ApolloProvider,
