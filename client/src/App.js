51f3cd1a7e227108660f7e0e98c346e6df9cd903
Sampreeti Das <sampreetighosh@gmail.com>
2022-04-25 19:53:50 +0100

    Update to the App Js file

--- client/src/App.js
+++ client/src/App.js
@@ -42,8 +42,8 @@ function App() {
         <>
           <Navbar />
           <Routes>
-            <Route path="/" element={<SearchBooks />} />
-            <Route path="/saved" element={<SavedBooks />} />
+            <Route path="/" component={SearchBooks} />
+            <Route path="/saved" component={SavedBooks} />
             <Route
               path="*"
               element={<h1 className="display-2">Wrong page!</h1>}
